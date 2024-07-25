4cac5ed3b53584ce60dd4b6e9251628bf44cff38
solo12mon <robertsolomon313@gmail.com>
2024-07-25 11:36:27 +0100

    my News app

---
+++ app/(content)/news/page.js
@@ -0,0 +1,11 @@
+import NewsList from "@/component/new-list";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1> News Page </h1>
+      <NewsList news={DUMMY_NEWS} />
+    </>
+  );
+}
